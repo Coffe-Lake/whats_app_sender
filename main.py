01e2b1dba78b1b7cecd93a2a9a9d84a30f5657f4
Coffe-Lake <felixkot00@gmail.com>
2022-08-06 21:39:02 +0300

    New repo

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import pywhatkit
+
+
+def send_message_inst():
+    """Мгновенная отправка сообщения через WhatsApp"""
+    
+    mobile = input("Введите номер получателя: ")
+    message = input("Введите текст сообщения")
+    
+    pywhatkit.sendwhatmsg_instantly(
+        phone_no=mobile, 
+        message=message
+        )
+
+
+def send_message_to_time():
+    """Отправка сообщения ко времени через WhatsApp"""
+    
+    mobile = input("Введите номер получателя: ")
+    message = input("Введите текст сообщения")
+    hour = int(input("Введите часы: "))
+    minutes = int(input("Введите минуты: "))
+ 
+    pywhatkit.sendwhatmsg(
+        phone_no=mobile, 
+        message=message, 
+        time_hour=hour, 
+        time_min=minutes
+        )
+
+
+def main():
+    # send_message_inst()
+    send_message_to_time()
+
+
+if __name__ == "__main__":
+    main()
